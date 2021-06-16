37641a37c314494401729b222b874f8ebf1fb44f
Altan Guc <altan.guc@colorado.edu>
2021-06-16 14:29:24 -0600

    adding up-to-date server.js file

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+var bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/public");
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(require("./routes/html-routes"));
+app.use(require("./routes/api-routes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
